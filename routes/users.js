ed52a41931b581e337f0a5d348e421a8d73a4c2e
Luis Paulo Silva Reis <48827159+Luis-Paulo-Silva-Reis@users.noreply.github.com>
2023-03-24 16:48:35 -0300

    v6
    v6

--- routes/users.js
+++ routes/users.js
@@ -9,54 +9,74 @@ const router = express.Router();
 const jwtSecret = process.env.JWT_SECRET;
 
 router.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  const hashedPassword = bcrypt.hashSync(password, 10);
-  const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
-  pool.query(sql, [name, email, hashedPassword], (err, result) => {
-    if (err) {
-      console.error("Error inserting user into database: " + err.stack);
-      res.status(500).json({ error: "Internal server error" });
+  try {
+    const { name, email, password } = req.body;
+
+    // Verifica se o e-mail já está cadastrado
+    const emailCheck = "SELECT COUNT(*) as count FROM users WHERE email = ?";
+    const [emailCheckResult] = await pool.query(emailCheck, [email]);
+    if (emailCheckResult[0].count > 0) {
+      res.status(400).json({ error: "Email already exists" });
       return;
     }
-    const userId = result.insertId;
+
+    // Cria hash da senha
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Insere usuário no banco de dados
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    const [insertResult] = await pool.query(sql, [
+      name,
+      email,
+      hashedPassword,
+    ]);
+    const userId = insertResult.insertId;
+
+    // Gera token JWT e retorna na resposta
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
       expiresIn: "24h",
     });
     res.json({ userId, name, email, token });
-  });
+  } catch (error) {
+    console.error("Error registering user: " + error.stack);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
-router.post("/auth", (req, res) => {
-  const { email, password } = req.body;
-  const sql = "SELECT id, name, email, password FROM users WHERE email = ?";
-  pool.query(sql, [email], async (err, results) => {
-    if (err) {
-      console.error("Error selecting user from database: " + err.stack);
-      res.status(500).json({ error: "Internal server error" });
+// Rota para autenticação de usuário
+router.post("/auth", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Verifica se o e-mail existe no banco de dados
+    const sql = "SELECT id, name, email, password FROM users WHERE email = ?";
+    const [results] = await pool.query(sql, [email]);
+    if (results.length !== 1) {
+      res.status(401).json({ error: "Invalid email or password" });
       return;
     }
-    if (results.length === 1) {
-      const passwordMatch = await bcrypt.compare(password, results[0].password);
-      if (passwordMatch) {
-        const token = jwt.sign(
-          { userId: results[0].id },
-          process.env.JWT_SECRET,
-          { expiresIn: "24h" }
-        );
 
-        res.json({
-          id: results[0].id,
-          name: results[0].name,
-          email: results[0].email,
-          token: token,
-        });
-      } else {
-        res.status(401).json({ error: "Invalid email or password" });
-      }
-    } else {
+    // Compara a senha digitada com a senha armazenada no banco de dados
+    const passwordMatch = await bcrypt.compare(password, results[0].password);
+    if (!passwordMatch) {
       res.status(401).json({ error: "Invalid email or password" });
+      return;
     }
-  });
+
+    // Gera token JWT e retorna na resposta
+    const token = jwt.sign({ userId: results[0].id }, process.env.JWT_SECRET, {
+      expiresIn: "24h",
+    });
+    res.json({
+      id: results[0].id,
+      name: results[0].name,
+      email: results[0].email,
+      token: token,
+    });
+  } catch (error) {
+    console.error("Error authenticating user: " + error.stack);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 router.get("/protected", verifyToken, (req, res) => {
